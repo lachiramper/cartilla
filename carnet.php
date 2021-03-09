2a705de41756b7428ded5c84fc5afa0a7d140678
AlberttoG2 <albertt0801.ag@gmail.com>
2021-03-08 23:43:00 -0600

    carnet.php

---
+++ carnet.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>Carnet</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col">
+            <div class="alert alert-success text-center">
+                <b>Cartilla de Vacunacion</b>
+            </div>
+        </div>
+    </div>
+    <div class="col-3"></div>
+</div>
+
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0"
+        crossorigin="anonymous"></script>
+</body>
+</html>
