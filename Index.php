cb2f846658bd084a03545e7257d231de81227600
alberto <albertt0801.ag@gmail.com>
2021-03-11 13:00:24 -0600

    visualizacion carnet.php

--- Index.php
+++ Index.php
@@ -21,71 +21,71 @@
                     <div class="card-content">
                         <div class="m-2">
                             <label for="curp" class="form-label">CURP</label>
-                            <input type="text" class="form-control" id="curp" required>
+                            <input type="text" class="form-control" id="curp" name="curp" required>
                         </div>
                         <div class="m-2">
                             <label for="apellidoPaterno" class="form-label">Apellido Paterno</label>
-                            <input type="text" class="form-control" id="apellidoPaterno" required>
+                            <input type="text" class="form-control" id="apellidoPaterno" name="apellidoPaterno" required>
                         </div>
                         <div class="m-2">
                             <label for="apellidoMaterno" class="form-label">Apellido Materno</label>
-                            <input type="text" class="form-control" id="apellidoMaterno" required>
+                            <input type="text" class="form-control" id="apellidoMaterno" name="apellidoMaterno" required>
                         </div>
                         <div class="m-2">
                             <label for="nombre" class="form-label">Nombre</label>
-                            <input type="text" class="form-control" id="nombre" required>
+                            <input type="text" class="form-control" id="nombre" name="nombre" required>
                         </div>
                         <div class="m-2">
                             <label for="matricula" class="form-label">Matricula</label>
-                            <input type="text" class="form-control" id="matricula" required>
+                            <input type="text" class="form-control" id="matricula" name="matricula" required>
                         </div>
                         <div class="m-2">
                             <label for="unidadMedica" class="form-label">Unidad Medica</label>
-                            <input type="text" class="form-control" id="unidadMedica" required>
+                            <input type="text" class="form-control" id="unidadMedica" name="unidadMedica" required>
                         </div>
                         <div class="m-2">
                             <label for="edad" class="form-label">Edad</label>
-                            <input type="text" class="form-control" id="edad" required>
+                            <input type="text" class="form-control" id="edad" name="edad" required>
                         </div>
                         <div class="m-2">
                             <label for="sexo" class="form-label">Sexo</label>
-                            <input type="text" class="form-control" id="sexo" required>
+                            <input type="text" class="form-control" id="sexo" name="sexo" required>
                         </div>
                         <div class="m-2">
                             <label for="domicilio" class="form-label">Domicilio</label>
-                            <input type="text" class="form-control" id="domicilio" required>
+                            <input type="text" class="form-control" id="domicilio" name="domicilio" required>
                         </div>
                         <div class="m-2">
                             <label for="calle" class="form-label">Calle</label>
-                            <input type="text" class="form-control" id="calle" required>
+                            <input type="text" class="form-control" id="calle" name="calle" required>
                         </div>
                         <div class="m-2">
                             <label for="colonia" class="form-label">Colonia</label>
-                            <input type="text" class="form-control" id="colonia" required>
+                            <input type="text" class="form-control" id="colonia" name="colonia" required>
                         </div>
                         <div class="m-2">
                             <label for="municipio" class="form-label">Municipio</label>
-                            <input type="text" class="form-control" id="municipio" required>
+                            <input type="text" class="form-control" id="municipio" name="municipio" required>
                         </div>
                         <div class="m-2">
                             <label for="cp" class="form-label">Codigo Postal</label>
-                            <input type="text" class="form-control" id="cp" required>
+                            <input type="text" class="form-control" id="cp" name="cp" required>
                         </div>
                         <div class="m-2">
                             <label for="entidadFederativa" class="form-label">Entidad Federativa</label>
-                            <input type="text" class="form-control" id="entidadFederativa" required>
+                            <input type="text" class="form-control" id="entidadFederativa" name="entidadFederativa" required>
                         </div>
                         <div class="m-2">
                             <label for="Fecha de registro" class="form-label">Fecha de Registro</label>
-                            <input type="text" class="form-control" id="Fecha de registro" required>
+                            <input type="text" class="form-control" id="Fecha de registro"  name="fechaRegistro" required>
                         </div>
                         <div class="m-2">
                             <label for="Estatura" class="form-label">Estatura</label>
-                            <input type="text" class="form-control" id="Fecha de registro" required>
+                            <input type="text" class="form-control" id="estatura" name="estatura" required>
                         </div>
                         <div class="m-2">
                             <label for="Peso" class="form-label">Peso</label>
-                            <input type="text" class="form-control" id="Peso" required>
+                            <input type="text" class="form-control" id="Peso" name="peso" required>
                         </div>
 
                     </div>
@@ -94,8 +94,7 @@
                 <div class="row">
                     <div class="col-8"></div>
                     <div class="col">
-                        <button class="btn btn-danger btn-sm">Cancelar</button>
-                        <button class="btn btn-primary btn-sm" value="guardar">Guardar</button>
+                        <button class="btn btn-primary btn-sm" value="send1">Guardar</button>
                     </div>
                 </div>
             </div>
--- carnet.php
+++ carnet.php
@@ -7,13 +7,59 @@
 </head>
 <body>
 <div class="container">
+
+    <?php
+        $curp = $_GET['curp'];
+        $apellidoPaterno = $_GET['apellidoPaterno'];
+        $apellidoMaterno = $_GET['apellidoMaterno'];
+        $nombre = $_GET['nombre'];
+        $matricula = $_GET['matricula'];
+        $unidadMedica = $_GET['unidadMedica'];
+        $edad = $_GET['edad'];
+        $sexo = $_GET['sexo'];
+        $domicilio = $_GET['domicilio'];
+        $calle = $_GET['calle'];
+        $colonia = $_GET['colonia'];
+        $municipio = $_GET['municipio'];
+        $cp = $_GET['cp'];
+        $entidadFederativa = $_GET['entidadFederativa'];
+        $fechaRegistro = $_GET['fechaRegistro'];
+        $estatura = $_GET['estatura'];
+        $peso = $_GET['peso'];
+    ?>
     <div class="row">
-        <div class="col-3"></div>
+        <div class="col-1"></div>
         <div class="col">
             <div class="alert alert-success text-center">
                 <b>Cartilla de Vacunacion</b>
             </div>
+            <ul class="list-group">
+                <li class="list-group-item active text-center"><b><?php echo  $apellidoPaterno, ' ', $apellidoMaterno,' ',$nombre?></b></li>
+                <li class="list-group-item"><b>CURP: </b><?php echo $curp?></li>
+                <li class="list-group-item"><b>Matricula: </b><?php echo $matricula?></li>
+                <li class="list-group-item"><b>Unidad de Medica: </b><?php echo $unidadMedica?></li>
+                <li class="list-group-item"><b>Edad: </b><?php echo $edad?></li>
+                <li class="list-group-item"><b>Sexo: </b><?php echo $sexo?></li>
+                <li class="list-group-item"><b>Domicilio: </b><?php echo $domicilio?></li>
+                <li class="list-group-item"><b>Calle: </b><?php echo $calle?></li>
+                <li class="list-group-item"><b>Colonia: </b><?php echo $colonia?></li>
+                <li class="list-group-item"><b>Municipio: </b><?php echo $municipio?></li>
+                <li class="list-group-item"><b>CP: </b><?php echo $cp?></li>
+                <li class="list-group-item"><b>Entidad de Federativa: </b><?php echo $entidadFederativa?></li>
+                <li class="list-group-item"><b>Fecha de Registro: </b><?php echo $fechaRegistro?></li>
+                <li class="list-group-item"><b>Estatura: </b><?php echo $estatura?></li>
+                <li class="list-group-item"><b>Peso: </b><?php echo $peso?></li>
         </div>
+        <form method="get" action="cartillaFormulario.php">
+        <div class="card-footer">
+            <div class="row">
+                <div class="col-8"></div>
+                <div class="col">
+                    <button class="btn btn-primary btn-sm" value="send1">Guardar</button>
+                </div>
+            </div>
+        </div>
+        </form>
     </div>
     <div class="col-3"></div>
 </div>
